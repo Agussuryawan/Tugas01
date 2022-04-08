4d41154511f73e657c082729a00d1c1b2a458e94
Agussuryawan <101536699+Agussuryawan@users.noreply.github.com>
2022-04-08 19:21:07 +0800

    Setor Tugas 1

---
+++ T1point01.java
@@ -0,0 +1,23 @@
+package t1point01;
+
+/**
+ *
+ * @author Suryawan 20101290
+ */
+public class T1point01 {
+
+    public static void main(String[] args) {
+        {
+        String [] nama={"I Ketut Agus Suryawan","I Wayan Mahendra Putra","I Made Adi Wiguna","I Komang Agus Santika", "I Kadek Wahyu Putra"};
+  
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+        System.out.println(nama[3]);
+        System.out.println(nama[4]);
+        }
+    }
+}
+    
+    
+
